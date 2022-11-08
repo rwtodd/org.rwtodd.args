72a6a06f5a6883e94e5c57b6f9a80194439aa992
Richard Todd <rwtodd@users.noreply.github.com>
2022-11-08 11:39:48 -0600

    added existing directory param type

--- src/main/java/org/rwtodd/args/ExistingDirectoryParam.java
+++ src/main/java/org/rwtodd/args/ExistingDirectoryParam.java
@@ -8,25 +8,26 @@ import java.util.Collection;
  * A param that takes expects an existing file on the filesystem
  * @author rwtodd
  */
-public class ExistingFileParam extends BasicOneArgParam<Path> {
+public class ExistingDirectoryParam extends BasicOneArgParam<Path> {
 
-    public ExistingFileParam(Collection<String> names, Path dflt, String help) {
+    public ExistingDirectoryParam(Collection<String> names, Path dflt, String help) {
         super(names, dflt, help);
     }
     
-    public ExistingFileParam(Collection<String> names, String help) {
-        this(names, null, help);
+    public ExistingDirectoryParam(Collection<String> names, String help) {
+        this(names, Path.of("."), help);
     }
     
     @Override
     protected Path convertArg(String param, String argument) throws ArgParserException {
         Path p = Path.of(argument);
         try {
-          if(!Files.isRegularFile(p))
-            throw new ArgParserException(String.format("Argument for <%s> is not an exisiting file!",param));
+          if(!Files.isDirectory(p))
+              throw new ArgParserException(String.format("Argument for <%s> is not an exisiting directory!",param));
         } catch(SecurityException se) {
           throw new ArgParserException(String.format("Argument for <%s> caused a security exception!",param), se);
         }
         return p;
     }
+    
 }
